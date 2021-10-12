d704fbb7f2b132f4fd0de332a55c478969799b84
Rob <maltese.rob@gmail.com>
2021-10-12 18:54:55 -0400

    Initial Commit

---
+++ LilWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LilWeather
+//
+//  Created by Rob Maltese on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LilWeather/LilWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilWeatherApp.swift
+//  LilWeather
+//
+//  Created by Rob Maltese on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LilWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
